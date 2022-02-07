4f3ebc2a0036948256ced19ae7d0e3bee1dfefb6
Tnovyloo <76777800+Tnovyloo@users.noreply.github.com>
2022-02-07 20:19:06 +0100

    Adding simple documentation and ENV module.

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import pydrive
+from os import environ
 from os import listdir
 from os.path import isfile, join
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-gauth = GoogleAuth()
-drive = GoogleDrive(gauth)
-mypath = "C:/YourPath/YourPath"
-photo_numer = 0
+drive_url = environ.get('URL')
+key = environ.get('GoogleAuth')
+google_auth = GoogleAuth(key)
+drive = GoogleDrive(google_auth)
+path = "C:/YourPath/YourPath"
+photo_number = 0
 
-files = [f"{mypath}/{file}" for file in listdir(mypath) if isfile(join(mypath, file))]
+files = [f"{path}/{file}" for file in listdir(path) if isfile(join(path, file))]
 print(files)
 
-
 for upload_file in files:
-    gfile = drive.CreateFile({'parents': [{'id': '1ciVEzXo6b-dKb99fDctGckLDKvyUjdYh'}]})
-    gfile.SetContentFile(upload_file)
-    gfile['title'] = f"{photo_numer}"
-    photo_numer += 1
-    gfile.Upload()
+    google_file = drive.CreateFile({'parents': [{'id': drive_url}]}) # Create file
+    google_file.SetContentFile(upload_file) # Set current file to 'SetContentFile'
+    google_file['title'] = f"{photo_number}" # Change file title to photo number
+    photo_number += 1 # Iterate number of photos
+    google_file.Upload() # Upload file
